a85c259e9fc5c9e9b8c65cf41e36e05424fde6e1
Mayumifavs <mayumsfavor@gmail.com>
2022-12-29 09:48:01 +0800

    First commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "fe11239015b0bb9939ab7a649b2cadcc"
+    }
+}
+
+module.exports = constants;
